f4863b6433c7ebb142ad971d0a079018dddba6cd
mishael <mishaelmishantoz@gmail.com>
2024-04-12 11:05:00 +0300

    fix the progressTrack component , eliminating the error , deployment not defined

--- src/variables/ProgressTrack.js
+++ src/variables/ProgressTrack.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {
+    Button,
     Card,
     CardHeader,
     CardBody,
     Col,
+    Row,
+    Table,
 } from "reactstrap";
 import Skeleton from 'react-loading-skeleton';
 import Api from 'views/dataviews/reduximplementation/Api';
@@ -28,58 +31,82 @@ const ProgressTrack = () => {
         const count = messageCounts[messageUuid];
         return { ...message, count };
     });
-    
-    // Display project name and theme for each recurring message object
-    projectInfo.forEach(project => {
-        console.log(`Project Name: ${project.playlist.deployment.project.projectName}`);
-        console.log("message title::",project.messagetitle);
-        console.log(`message Count: ${project.count} times`);
-        console.log(`Theme URL: ${project.playlist.deployment.project.theme}`);
-        
-        
-    });
 
     return (
         <Col className="mb-5 mb-xl-0" xl="4">
             <Card className="shadow">
                 <CardHeader className="border-0">
-                    <h3 className="mb-0">Progress track</h3>
+                    <Row className="align-items-center">
+                        <div className="col">
+                            <h3 className="mb-0">Progress track</h3>
+                        </div>
+                        <div className="col text-right">
+                            <Button
+                                color="primary"
+                                href="#pablo"
+                                onClick={(e) => e.preventDefault()}
+                                size="sm"
+                            >
+                                See all
+                            </Button>
+                        </div>
+                    </Row>
                 </CardHeader>
-                <CardBody>
-                    {/* Render skeleton loading if data is loading */}
-                    {isLoading ? (
-                        <Skeleton height={100} count={4} />
-                    ) : (
-                        <ul className="list my--3 list-group list-group-flush">
-                            {/* Map over projectInfo to generate progress bars dynamically */}
-                            {projectInfo.map((project, index) => (
-                                <li className="px-0 list-group-item" key={index}>
-                                    <div className="align-items-center row">
-                                        <div className="col-auto col">
+                <Table className="align-items-center table-flush" responsive>
+                    <thead className="thead-light">
+                        <tr>
+                            <th scope="col">Project</th>
+                            <th scope="col">Messages</th>
+                            <th scope="col">Progress</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {isLoading ? (
+                            // Render skeleton loading if data is loading
+                            <tr>
+                                <td colSpan="3">
+                                    <Skeleton height={100} count={4} />
+                                </td>
+                            </tr>
+                        ) : (
+                            // Map over projectInfo to generate table rows dynamically
+                            projectInfo.map((project, index) => (
+                                <tr key={index}>
+                                    <td>
+                                        <div className="d-flex align-items-center">
                                             <a className="avatar rounded-circle" href="#pablo">
-                                                <img alt="Project Theme" src={project.playlist.deployment.project.theme} />
+                                                <img 
+                                                    src={project.playlist?.deployment?.project?.theme} 
+                                                    alt="Project Theme" 
+                                                    className="img-fluid rounded-circle" 
+                                                    style={{ width: '40px', height: '40px' }} 
+                                                />
                                             </a>
+                                            <span className="ml-3">{project.messageuuid || 'No Message uuid'}</span>
+                                            <span className="ml-3">{project.playlist?.deployment?.project?.projectName || 'No project name'}</span>
                                         </div>
-                                        <div className="col">
-                                            <h5>{project.playlist.deployment.project.projectName}</h5>
-                                            <div className="progress-xs mb-0 progress">
-                                                {/* Replace progress bar with project progress information */}
-                                                <div className="progress-bar bg-orange"
-                                                    role="progressbar"
-                                                    aria-valuenow="60"
-                                                    aria-valuemin="0"
-                                                    aria-valuemax="100"
-                                                    style={{ width: '60%' }}
-                                                >
-                                                </div>
-                                            </div>
+                                    </td>
+                                    <td>{project.count}</td>
+                                    <td>
+                                        <div className="progress-xs mb-0 progress">
+                                            <div
+                                                className="progress-bar bg-orange"
+                                                role="progressbar"
+                                                aria-valuenow="60"
+                                                aria-valuemin="0"
+                                                aria-valuemax="100"
+                                                style={{ width: '60%' }}
+                                            ></div>
                                         </div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </CardBody>
+                                    </td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </Table>
+                {!isLoading && projectInfo.length === 0 && (
+                    <CardBody>No recurring projects found.</CardBody>
+                )}
             </Card>
         </Col>
     );
